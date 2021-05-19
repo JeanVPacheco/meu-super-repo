a4cf78d439f07a04590b383cf9020bd9c786abd5
Jean V  Pacheco <jeanvitorp@gmail.com>
2021-05-19 18:37:05 -0300

    Adiciona até o 5º exercício

--- bloco_4/dia_1/exercicio.js
+++ bloco_4/dia_1/exercicio.js
@@ -1,13 +1,45 @@
-var a = 5;
-var b = 2;
+let a = 5;
+let b = 2;
+let c = 1;
 
-// adição
-console.log(+a+b)
-// subtração
-console.log(a-b);
-// multiplicação
-console.log(a*b);
-// divisão
-console.log(a/b);
-//módulo
-console.log(a%b)
+// Adição
+// console.log(+a+b)
+
+// Subtração
+// console.log(a-b);
+
+// Multiplicação
+// console.log(a*b);
+
+// Divisão
+// console.log(a/b);
+
+// Módulo
+// console.log(a%b)
+
+// Retorna o maior número
+// if (a > b){
+//   console.log(a)
+// } else {
+//   console.log(b)
+// }
+
+// Compara três números
+// if (a>b && a>c){
+//   console.log(a)
+// }
+// else if (b>a && b>c){
+//   console.log(b)
+// } else {
+//   console.log(c)
+// }
+
+// Verifica se os ângulos correspondem a um triângulo
+// let agl_um = 30;
+// let agl_dois = 75;
+// let agl_tres = 75;
+
+// if (agl_um < 1 || agl_dois < 1 || agl_tres < 1) {
+//   console.log("Erro, um ângulo é inválido")
+// } else {
+// console.log (Boolean (agl_um + agl_dois + agl_tres == 180));}
