332d71ffdbc755f774ddc00a2e504c40830fb6e9
Jean V  Pacheco <jeanvitorp@gmail.com>
2021-05-19 16:47:53 -0300

    Primeiro exercício

---
+++ bloco_4/dia_1/exercicio.js
@@ -0,0 +1,13 @@
+var a = 5;
+var b = 2;
+
+// adição
+console.log(+a+b)
+// subtração
+console.log(a-b);
+// multiplicação
+console.log(a*b);
+// divisão
+console.log(a/b);
+//módulo
+console.log(a%b)
